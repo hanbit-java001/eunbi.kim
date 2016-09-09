7ea6175924ca666628d5ffb4e0cb69792b516cc4
hb5012 <hb5012@hb5012>
2016-09-09 12:04:00 +0900

    패키지 변경

---
+++ src/main/java/com/hanbit/eunbi/kim/standalone/Hello.java
@@ -0,0 +1,8 @@
+package com.hanbit.eunbi.kim.standalone;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		System.out.println("How are you?");
+	}
+}
