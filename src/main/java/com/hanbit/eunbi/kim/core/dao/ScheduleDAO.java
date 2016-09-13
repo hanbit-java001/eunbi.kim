ba3aa73b039bad789bd9224f235cfee7b66603b8
hb5012 <hb5012@hb5012>
2016-09-13 14:39:55 +0900

    selectList

--- src/main/java/com/hanbit/eunbi/kim/core/dao/ScheduleDAO.java
+++ src/main/java/com/hanbit/eunbi/kim/core/dao/ScheduleDAO.java
@@ -21,11 +21,10 @@ public class ScheduleDAO {
 
 		}
 
-
 		return connection;
 	}
 
-	private void closeConnection(Connection connection){
+	private void closeConnection(Connection connection) {
 		try {
 			connection.close();
 		} catch (SQLException e) {
@@ -33,13 +32,13 @@ public class ScheduleDAO {
 		}
 	}
 
-	private int executeSql(Connection connection, String sql, List params){
+	private int executeSql(Connection connection, String sql, List params) {
 		int result = 0;
 
 		try {
 			PreparedStatement statement = connection.prepareStatement(sql);
 
-			for(int i=0;i<params.size();i++){
+			for (int i = 0; i < params.size(); i++) {
 				statement.setObject(i + 1, params.get(i));
 			}
 
@@ -103,4 +102,51 @@ public class ScheduleDAO {
 
 		return result;
 	}
+
+	public List<ScheduleVO> selectSchedules(String startDt, String endDt) {
+		Connection connection = getConnection();
+
+		String sql = "SELECT SHCEDULE_ID, TITLE, MEMO, START_DT, END_DT FROM SCHEDULE "
+				+ "WHERE START_DT <= ? AND END_DT >= ?";
+
+		List params = new ArrayList();
+		params.add(endDt);
+		params.add(startDt);
+
+		List<ScheduleVO> result = new ArrayList<ScheduleVO>();
+
+		try {
+			PreparedStatement statement = connection.prepareStatement(sql);
+
+			for (int i = 0; i < params.size(); i++) {
+				statement.setObject(i + 1, params.get(i));
+			}
+
+			ResultSet resultSet = statement.executeQuery();
+
+			while (resultSet.next()) {
+				ScheduleVO schedule = new ScheduleVO();
+
+				schedule.setScheduleId(resultSet.getString("SCHEDULE_ID"));
+				schedule.setTitle(resultSet.getString("TITLE"));
+				schedule.setMemo(resultSet.getString("MEMO"));
+				schedule.setStartDt(resultSet.getString("START_DT"));
+				schedule.setEndDt(resultSet.getString("END_DT"));
+
+				result.add(schedule);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		closeConnection(connection);
+
+		return result;
+	}
+
+	public ScheduleVO selectSchedule(String scheduleId) {
+		return null;
+	}
+
 }
--- src/main/java/com/hanbit/eunbi/kim/core/service/SchedulerService.java
+++ src/main/java/com/hanbit/eunbi/kim/core/service/SchedulerService.java
@@ -10,29 +10,27 @@ import java.util.List;
 import com.hanbit.eunbi.kim.core.dao.ScheduleDAO;
 import com.hanbit.eunbi.kim.core.vo.ScheduleVO;
 
-
 public class SchedulerService {
-	public int addSchedule(ScheduleVO schedule){
-		ScheduleDAO scheduleDAO = new ScheduleDAO();
+	private ScheduleDAO scheduleDAO = new ScheduleDAO();
 
+	public int addSchedule(ScheduleVO schedule) {
 		return scheduleDAO.insertSchedule(schedule);
 	}
 
-	public int modifySchedule(ScheduleVO schedule){
-		return 0;
+	public int modifySchedule(ScheduleVO schedule) {
+		return scheduleDAO.updateSchedule(schedule);
 	}
 
-	public int removeSchedule(String scheduleID){
-		return 0;
+	public int removeSchedule(String scheduleId) {
+		return scheduleDAO.deleteSchedule(scheduleId);
 	}
 
 	public List<ScheduleVO> listSchedules(String startDt, String endDt){
-		return null;
+		return scheduleDAO.selectSchedules(startDt, endDt);
 	}
 
-	public ScheduleVO getSchedule(String scheduleID){
-		return null;
+	public ScheduleVO getSchedule(String scheduleId) {
+		return scheduleDAO.selectSchedule(scheduleId);
 	}
-
 
 }
